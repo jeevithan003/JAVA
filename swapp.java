f88777b9ce5c6cc90c8a436e162132462c74ffab
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-04-21 09:17:40 +0530

    Create swapp.java

---
+++ swapp.java
@@ -0,0 +1,20 @@
+// the swappinf beteen the two variables in java
+//simple temp varible.
+class Main{
+    public static void main(String[] args) {
+
+int a=24;
+int c=56;
+        System.out.println("before a value:"+a);
+        System.out.println("before c value :"+c);
+int temp;
+temp=a;
+a=c;
+c=temp;
+        System.out.println("after the a value "+a);
+        System.out.println("after the b value "+c);
+
+    }
+
+
+}
