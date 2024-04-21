340871cb8af2b7f91a461e5c9feb6afb9d76ca18
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-04-21 09:13:18 +0530

    Create datatypes.java

---
+++ datatypes.java
@@ -0,0 +1,20 @@
+
+//the datatpye in java
+//basics data types in java
+class Main{
+    public static void main(String[] args) {
+        int a=13;
+        String name="jeevithan  iis java developer";
+        double accuracy=934.56;
+        boolean isgoal=true;
+        char n='%';
+        System.out.println("your name is "+name);
+        System.out.println("your avvuracy is "+accuracy);
+        System.out.println("the boolen is could be "+isgoal);
+
+
+
+    }
+
+
+}
