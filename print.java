e2cf69caa14a875e85c1f5aefb68577319ea6db3
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-04-21 09:02:13 +0530

    Create print.java

---
+++ print.java
@@ -0,0 +1,13 @@
+
+//  my first java program
+//printing output to the terminal
+class Main{
+    public static void main(String[] args) {
+        System.out.println("hello bro code");
+        System.out.println("I AM A BEGINER JAVA DEVELOPER");
+
+
+    }
+
+
+}
