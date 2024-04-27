873d2eb35c082957502979eacde90345077279ea
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-04-27 16:53:17 +0530

    Create userinput.java

---
+++ userinput.java
@@ -0,0 +1,25 @@
+// the swappinf beteen the two variables in java
+//simple temp varible.
+ import java.util.Scanner;
+class Main{
+    public static void main(String[] args) {
+        int age;
+
+        Scanner sc =new Scanner((System.in));
+        System.out.println("enter the name");
+        String name= sc.nextLine();
+        System.out.println("enter the age");
+        age=sc.nextInt();
+
+        System.out.println("your name is "+name);
+        System.out.println("your age is "+age);
+
+
+
+
+
+
+    }
+
+
+}
