4a366d4a2714f9ee22fd936d7130e6c5587262ae
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-04-29 20:59:24 +0530

    Create ifelse.java

---
+++ ifelse.java
@@ -0,0 +1,22 @@
+// the swappinf beteen the two variables in java
+//simple temp varible.
+ import java.util.Scanner;
+import javax.swing.JOptionPane;
+class Main{
+    public static void main(String[] args) {
+
+                String str = "GeeksforGeeks";
+                int i = 4;
+
+                // if block
+                if (i == 4) {
+                    i++;
+                    System.out.println(str);
+                }
+
+                // Executed by default
+                System.out.println("i = " + i);
+            }
+        }
+
+ 
